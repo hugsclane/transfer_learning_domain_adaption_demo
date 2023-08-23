688702072eb2b6004da0a7cd1185a629ef431eb8
huganardo <mail.hugocl@gmail.com>
2023-08-23 17:08:08 +1000

    Updated README + started preprocess_source_dom()
    added revlo_util and policy code to the dataframe.

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import shap
 import pytest
+import numpt as np
 import pandas as pd
 from scipy import stats
 import random as rd
@@ -7,7 +8,7 @@ import os
 from keras.utils import plot_model
 
 def main():
-    preprocess()
+    preprocess_source_dom()
 
 ## Takes csv files in
 def data_to_df(filename): #modify if we need more df inputs that arent data.
@@ -20,12 +21,27 @@ def data_to_df(filename): #modify if we need more df inputs that arent data.
         Print("{} is not a csv",filename)
     return df
 
-def preprocess(): ##:TODO Preprocessing is based on steps from https://gitlab.com/richdataco/rdc-public/rdc-ic/research/transfer-learning/ecmlpkdd2019/-/blob/master/preprocess_lending_club_files.R?ref_type=heads.
+def preprocess_source_dom(): ##:TODO Preprocessing is based on steps from https://gitlab.com/richdataco/rdc-public/rdc-ic/research/transfer-learning/ecmlpkdd2019/-/blob/master/preprocess_lending_club_files.R?ref_type=heads.
     rd.seed(54)
     df = data_to_df('loans_full_schema.csv')
-
+    df['revol_util'] = (df['total_credit_limit'] - df['total_credit_utilized']) / df['credit_limit'] #:TODO test this to see if balance = total_credit_limit - total_credit_utilized. 
+                                                                                                     #if not, check balance < total_credit_limit - total_credit_utilized so see if balance is one one loan.
+    df['policy_code'] = df.apply(lambda x: 2 if ( #:TODO test this to see if 1. the lambda functions works, 2. that policy_code 2 exits in the dataset.
+        df['total_credit_utilized'].isna() and
+        df['total_credit_limit'].isna() and
+        df['open_credit_lines'].isna() and
+        df['total_credit_lines'].isna() and 
+        df['grade'] in ['F', 'G'] and
+        23.5 <= df['int_rate'] <= 26.06 and
+        df['loan_term'] == 36 and
+        df['loan_amount'] <= 15000
+        ) else 1, axis=1)  
     col_ls = ['issue_month','state','total_credit_limit','term','sub_grade',\
-              'revol_util','intrest_rate','installment','grade']
+              'revol_util','intrest_rate','installment','grade','emp_length',\
+              'debt_to_income','balance','total_credit_utilized','accounts_opened_24m',\
+              'loan_status', 'loan_purpose','application_type','annual_income','loan_amount']
+
+
     return df
 
 
