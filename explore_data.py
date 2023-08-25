d1e909602fa5f428ea348401208836e504817f9a
huganardo <mail.hugocl@gmail.com>
2023-08-25 17:17:47 +1000

    Updated README.md, Added explore data to test preprocessing results,
    added some more preprocessing steps, noted changes in readme and created a explore_data.py file for testing.

---
+++ explore_data.py
@@ -0,0 +1,43 @@
+from model import data_to_df
+import numpy as np
+import pandas as pd
+def main():
+    '''
+    Exploring data without preprocessing
+    '''
+    #data_no_pp = data_to_df("loans_full_schema.csv")
+    #check_policy_code(data_no_pp)
+    #unique_values_in_(data_no_pp)
+    #print(data_no_pp.dtypes)
+    '''
+    Exploring data with preprocessing
+    '''
+    data_with_pp= data_to_df("loans_full_schema_trimmed_normal_data.csv")
+    unique_values_in_(data_with_pp)
+    print(data_with_pp.dtypes)
+
+'''
+Check data for policy_code 2 values.
+'''
+def check_policy_code(data):
+    ##checking data['policy_code'].unique() there are no policy code 2 borrowers in the dataset
+    data['policy_code'] = data.apply(lambda x: 2 if (
+        x['total_credit_utilized']== np.NaN and
+        x['total_credit_limit']== np.NaN and
+        x['open_credit_lines']== np.NaN and
+        x['total_credit_lines']== np.NaN and
+        x['grade'] in ['F', 'G'] and
+        23.5 <= x['int_rate'] <= 26.06 and
+        x['loan_term'] == 36 and
+        x['loan_amount'] <= 15000
+        ) else 1, axis=1)
+    print(data['policy_code'].unique())
+
+def unique_values_in_(data):
+    columns_to_print = ['grade', 'sub_grade', 'interest_rate', 'installment', 'emp_length', 'debt_to_income', 'balance', 'total_credit_utilized', 'accounts_opened_24m', 'annual_income', 'loan_status']
+    for column in columns_to_print:
+        unique_values = data[column].unique()
+        print(f"Unique datapoints in {column}:\n", unique_values)
+
+if __name__ =="__main__":
+    main()
--- model.py
+++ model.py
@@ -6,69 +6,137 @@ from scipy import stats
 import random as rd
 import os
 from keras.utils import plot_model
+data_dir_path = os.path.join(os.path.dirname(os.path.abspath(__file__)) ,'data')
 
 def main():
-    data = preprocess_df()
-    model_something(data,'debt_consolidation','credit_card')
+    pre_pro_mod = True #serves as a way to indicate if preprocessing has been modified so as to regenerate data
+    data = preprocess_df(pre_pro_mod)
+    #model_something(data,'debt_consolidation','credit_card')
     #print(data['loan_purpose'].value_counts())
 
 ## Takes csv files in
 def data_to_df(filename): #modify if we need more df inputs that arent data.
+    '''
+    Load the dataframe
+    this function can only take properly formatted CSV files.
+    '''
     try:
-        dir_path = os.path.join(os.path.dirname(os.path.abspath(__file__)) ,'data',filename)
-        df = pd.read_csv(dir_path)
+        file_path = os.path.join(data_dir_path,filename)
+        return pd.read_csv(file_path)
     except FileNotFoundError:
         print("{} not found in data, or does not exist.",filename)
     except:
-        Print("{} is not a csv",filename)
-    return df
-
-def preprocess_df():
-    ## Preprocessing is based on steps from https://gitlab.com/richdataco/rdc-public/rdc-ic/research/transfer-learning/ecmlpkdd2019/-/blob/master/preprocess_lending_club_files.R?ref_type=heads.
-    df = data_to_df('loans_full_schema.csv')
-    df['revol_util'] = ((df['total_credit_utilized']) / df['total_credit_limit'] )
-    df['policy_code'] = df.apply(lambda x: 2 if (
-        x['total_credit_utilized']== np.NaN and
-        x['total_credit_limit']== np.NaN and
-        x['open_credit_lines']== np.NaN and
-        x['total_credit_lines']== np.NaN and
-        x['grade'] in ['F', 'G'] and
-        23.5 <= x['int_rate'] <= 26.06 and
-        x['loan_term'] == 36 and
-        x['loan_amount'] <= 15000
-        ) else 1, axis=1)
-
-    ##checking data['policy_code'].unique() there are no policy code 2 borrowers in the dataset
-    col_ls = ['issue_month','state','total_credit_limit','term','sub_grade',\
-              'revol_util','interest_rate','installment','grade','emp_length',\
-              'debt_to_income','balance','total_credit_utilized','accounts_opened_24m',\
-              'loan_status', 'loan_purpose','application_type','annual_income','loan_amount']
-    return df[col_ls]
-
-def model_something(data,sName,tName):
-    spl_ratio = 0.75
-    s_df,t_df = source_target_split(data,sName,tName,spl_ratio)
+        Print("{} is not a csv, or is not properly formatted",filename)
     return
 
+def preprocess_df(pre_pro_mod):
+    ## Preprocessing is based on steps from https://gitlab.com/richdataco/rdc-public/rdc-ic/research/transfer-learning/ecmlpkdd2019/-/blob/master/preprocess_lending_club_files.R?ref_type=heads.
+    input_file_name = 'loans_full_schema'
+    '''
+    When pre_pro_mod is False, preprocess_df will return previously generated data.
+    '''
+    if os.path.isfile(input_file_name + '_trimmed_normal_data.csv') and not pre_pro_mod:
+        return data_to_df(input_file_name +'_trimmed_normal_data')
+    std_trim = 10000 #seems aribtrary,:TODO ask about it?
+    df = data_to_df(input_file_name + ".csv")
+
+    '''
+    Map grade to integers
+    '''
+    grade_mapping = {
+        "A": 7,
+        "B": 6,
+        "C": 5,
+        "D": 4,
+        "E": 3,
+        "F": 2,
+        "G": 1
+    }
+
+    df['grade'] = df['grade'].map(lambda x: grade_mapping.get(x))
+
+    '''
+    Map sub_grade to integers
+    '''
+    sub_grade_mapping = {
+    '1':9,
+    '2':7,
+    '3':5,
+    '4':3,
+    '5':1
+    }
+
+    df['sub_grade'] = df['sub_grade'].map(lambda x: grade_mapping.get(x[0])*10 +sub_grade_mapping.get(x[1]))
+
+    '''
+    Generate cover column
+    '''
+    df['cover'] = df['annual_income'] / df['loan_amount'] 
+
+
+    '''
+    Generate revol_util column
+    '''
+    df['revol_util'] = ((df['total_credit_utilized']) / df['total_credit_limit'] )
+
+
+    '''
+    Map emp_length to floats
+    '''
+    df.loc[np.isnan(df['emp_length']), 'emp_length'] = 0
+    
+    '''
+    Map loan outcomes to binary outcomes
+    '''
+    df['loan_status'] = np.where(df['loan_status'] == 'Fully Paid', 1, 0)
+    
+    '''
+    Data columns to be included in the model.
+    '''
+    col_ls = ['issue_month','state','total_credit_limit','term','sub_grade',\
+            'revol_util','interest_rate','installment','grade','emp_length',\
+            'debt_to_income','balance','total_credit_utilized','accounts_opened_24m',\
+            'loan_status', 'loan_purpose','application_type','annual_income','loan_amount','cover']
+    '''
+    Normaliztion and standard deviation trim.
+    '''
+    #:TODO trim_function = lambda col:
+    # normalize_function = lambda col: (col - col.min()) / \
+    #     (col.max() - col.min() if col.max() - col.min() != 0 else 1)
+    #     norm_df_ df[col_ls].apply(trim_function).apply(normalize_function)
+    df[col_ls].to_csv(os.path.join(data_dir_path, input_file_name + '_trimmed_normal_data'+".csv"), sep = ',',mode= 'w')
+    return df[col_ls]
+
 def source_target_split(data,sName,tName,spl_ratio):
+    rand_seed = 54
+    '''
+    Function that splits data for PSC,
+    returns source and target splits.
+    '''
     df_size =  len(data[data['loan_purpose'] == sName].axes[0]) if len(data[data['loan_purpose'] == sName].axes[0])< \
         len(data[data['loan_purpose'] == tName].axes[0]) else len(data[data['loan_purpose'] ==tName].axes[0]) #checks which loan_purpose is smaller then uses that as the maximum size for the sum of samples.
-    s_df = data[data['loan_purpose'] == sName].sample(n = round(df_size*spl_ratio) , random_state=54)
-    #outcome are, n = df_size, n =
-    t_df = data[data['loan_purpose'] == tName].sample(n = round(df_size*(1-spl_ratio)),  random_state=54)
+    s_df = data[data['loan_purpose'] == sName].sample(n = round(df_size*spl_ratio) , random_state=rand_seed)
+    t_df = data[data['loan_purpose'] == tName].sample(n = round(df_size*(1-spl_ratio)),  random_state=rand_seed)
+    '''
+    Will fail if sampling did not occur correctly.
+    '''
     assert(df_size == len(s_df.axes[0]) + len(t_df.axes[0]))
     return s_df,t_df
 
 
+def model_something(data,sName,tName):
+    spl_ratio = 0
+    s_df,t_df = source_target_split(data,sName,tName,spl_ratio)
+    return
 
-'''
+''' BELOW ARE NOTES FOR DEVLOPMENT DOWN THE LINE, YOU CAN IGNORE THIS
 use KS to quant delta source(sd) and target domain(ts).
 beyond some threshold(thresh) of delta source and target domain, select those features for domain adaption
 (da_feat_dict).
 
 include other highly correlated features(cor_feat) for each selected features(key from da_feat_dict:  val from cor_feat),
 populate da_feat_dict, with correlated features.
-''' 
+'''
 
 #:TODO find the correct combinations by testing model accuracy (weighting correlated features or adding/removing them?)
 
